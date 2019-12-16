f4ae7f52f466da23f7999ade8289668375fe9f27
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2019-12-16 10:06:42 -0500

    Create a factorial function

--- js/factorial.js
+++ js/factorial.js
@@ -1,1 +1,12 @@
-alert("Sara te amo!!!");
+function factorial(number){
+  if (number == 0) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+}
+
+const numberToFactorial = 11;
+const factorialNumber = factorial(numberToFactorial);
+
+console.log("El factorial de " + numberToFactorial + " es igual a " + factorialNumber);
